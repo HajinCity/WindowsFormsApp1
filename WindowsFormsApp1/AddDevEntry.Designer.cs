f1f5121f445195926c39052ad46d91cb5256b689
HajinCity <128238629+HajinCity@users.noreply.github.com>
2025-11-24 19:25:16 +0800

    11-24-2025

--- WindowsFormsApp1/AddDevEntry.Designer.cs
+++ WindowsFormsApp1/AddDevEntry.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // AddDevEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1168, 924);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AddDevEntry";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AddDevEntry";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
