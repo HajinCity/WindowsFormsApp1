387eb4f63126b1f15401e926681fb8a0b6fcc9e2
HajinCity <128238629+HajinCity@users.noreply.github.com>
2025-11-24 18:04:40 +0800

    11-24-2025

--- WindowsFormsApp1/ViewGJEntry.Designer.cs
+++ WindowsFormsApp1/ViewGJEntry.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ViewGJEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1226, 624);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ViewGJEntry";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ViewGJEntry";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
