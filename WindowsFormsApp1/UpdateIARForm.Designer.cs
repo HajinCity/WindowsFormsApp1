488956b0d49f7f249a81c0e466c582c5cfe24a4f
HajinCity <128238629+HajinCity@users.noreply.github.com>
2025-11-26 03:35:46 +0800

    11-24-20258

--- WindowsFormsApp1/UpdateIARForm.Designer.cs
+++ WindowsFormsApp1/UpdateIARForm.Designer.cs
@@ -210,7 +210,7 @@
             this.panel1.Controls.Add(this.label19);
             this.panel1.Location = new System.Drawing.Point(43, 1216);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1073, 96);
+            this.panel1.Size = new System.Drawing.Size(1073, 147);
             this.panel1.TabIndex = 72;
             // 
             // pictureBox1
