c815a820cf4a39a2e21268cb198739de1e235751
HajinCity <128238629+HajinCity@users.noreply.github.com>
2025-11-26 11:47:32 +0800

    11-25-2025

--- WindowsFormsApp1/UpdateJEV.Designer.cs
+++ WindowsFormsApp1/UpdateJEV.Designer.cs
@@ -554,9 +554,9 @@
             this.label3.ForeColor = System.Drawing.Color.Black;
             this.label3.Location = new System.Drawing.Point(12, 9);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(143, 24);
+            this.label3.Size = new System.Drawing.Size(247, 24);
             this.label3.TabIndex = 135;
-            this.label3.Text = "Add New JEV";
+            this.label3.Text = "Update JEV Information";
             // 
             // UpdateJEV
             // 
