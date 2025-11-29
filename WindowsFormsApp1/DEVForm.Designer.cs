17b97ebb276d557ff611a5e4bca6fc5463dce621
HajinCity <128238629+HajinCity@users.noreply.github.com>
2025-11-29 21:48:39 +0800

    11-29-2025

--- WindowsFormsApp1/DEVForm.Designer.cs
+++ WindowsFormsApp1/DEVForm.Designer.cs
@@ -124,7 +124,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(24, 227);
+            this.label4.Location = new System.Drawing.Point(24, 232);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(174, 18);
             this.label4.TabIndex = 19;
